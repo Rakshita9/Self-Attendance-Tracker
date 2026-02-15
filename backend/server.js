db99fed34ec18c1b323a94498e221128ae718fba
Rakshita Agarwal <rakshitaa88@gmail.com>
2026-02-16 00:05:10 +0530

    Update cors policy

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ dotenv.config();
 
 
 //  Middleware
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: "*", credentials: true }));
 app.use(bodyParser.json());
 
 
